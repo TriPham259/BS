8016008f74bd15da58cad73da894efb1bea975b9
Tri Pham <tripham@Tris-MacBook-Pro.local>
2018-05-20 09:37:02 +0200

    refactor + add compatibility for mac & window

--- BSP1/BSP1_2/try_host.sh
+++ BSP1/BSP1_2/try_host.sh
@@ -14,11 +14,13 @@ OPTIONEN:
 
 ping_check(){
 while :; do
-	if ! ping -c 1 -w 1 $ADDRES &> /dev/null; then # -c=Anzahl; -w=AntwortZeit
-													# /dev/null= Pseudodatei, in die man alle Nachrichten und Meldungen von ping hinwerfen 
-		echo $ADDRES FAILED	
+	if ! ping -c 1 -W 1 $HOST &> /dev/null; then 
+	# -c: Anzahl der Abfragen an Server
+	# -w: AntwortZeit
+	# /dev/null= Pseudodatei, in die man alle Nachrichten und Meldungen von ping hinwerfen 
+		echo $HOST FAILED	
 	else
-		echo $ADDRES OK
+		echo $HOST OK
 fi
 sleep $PAUSE;
 done
@@ -31,26 +33,26 @@ case "$1" in
 	-h) usage
 			exit 0
 	;;
-	-s) if [ $# -eq 3 ]; then #wenn 3 Parameter
-				PAUSE=$2	#Ping-Intervall in Sekunden
-				ADDRES=$3	#Ziel-Addresse
-			else		#Abbruch, weil fehlerhafte Parameterangaben
-				usage
-				exit 1 
-			fi
+	-s) if [ $# -eq 3 ]; then 	# wenn 3 Parameter
+			PAUSE=$2			# Ping-Intervall in Sekunden = 2.Parameter von Tastatur
+			HOST=$3				# Ziel-Addresse = 3.Parameter von Tastatur
+		else
+			usage
+			exit 1 
+		fi
+		ping_check
+		exit 0
+	;;
+	*)	if [ $# -eq 1 ]; then 	# wenn 1 Parameter vorhanden aber nicht -h und -s
+			PAUSE=10 			# default Ping-Intervall in Sekunden 
+			HOST=$1				# Ziel-Addresse = (1.)Parameter von Tastatur
 			ping_check
 			exit 0
-	;;
-	*)	if [ $# -eq 1 ]; then #wenn 1 Parameter vorhanden aber nicht -h und -s
-				PAUSE=10 	#Ping-Intervall in Sekunden
-				ADDRES=$1	#Ziel-Addresse
-				ping_check
-				exit 0
-			fi
-			#Hilfe ausgeben, da keine Parameterangaben
-				usage
-				exit 1
+		fi
+		#Hilfe ausgeben wenn keine Parameterangaben ($# -eq 0)
+		usage
+		exit 1
 			
-			exit 0
+		exit 0
 esac
 
